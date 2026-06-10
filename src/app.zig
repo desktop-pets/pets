918da4509a189165b0313388b952056a9aa8eb25
Amin <aminntfnd@gmail.com>
2026-06-10 17:41:33 +0330

    move sprite animation logic to its own type

--- src/app.zig
+++ src/app.zig
@@ -30,6 +30,37 @@ const AppConfig = struct {
     }
 };
 
+const SpriteAnim = struct {
+    rows: i32,
+    cols: i32,
+    frame_idx: i32,
+    frame_count: i32,
+    frame_rect: rl.Rectangle,
+    texture: rl.Texture,
+
+    pub fn init(texture_path: [:0]const u8, frame_count: i32, rows: i32, cols: i32) !SpriteAnim {
+        const texture = try rl.Texture.init(texture_path);
+        const frame_rect = rl.Rectangle{
+            .x = 0,
+            .y = 0,
+            .width = @as(f32, @floatFromInt(@divFloor(texture.width, cols))),
+            .height = @as(f32, @floatFromInt(@divFloor(texture.height, rows))),
+        };
+        return .{ .frame_count = frame_count, .rows = rows, .cols = cols, .frame_idx = 0, .frame_rect = frame_rect, .texture = texture };
+    }
+    pub fn deinit(self: SpriteAnim) void {
+        rl.unloadTexture(self.texture);
+    }
+    pub fn advance(self: *SpriteAnim) void {
+        self.frame_idx = @rem(self.frame_idx + 1, self.frame_count);
+    }
+    pub fn draw(self: *SpriteAnim, position: rl.Vector2, tint: rl.Color) void {
+        self.frame_rect.x = @as(f32, @floatFromInt(@rem(self.frame_idx, self.cols))) * self.frame_rect.width;
+        self.frame_rect.y = @as(f32, @floatFromInt(@divFloor(self.frame_idx, self.cols))) * self.frame_rect.height;
+        self.texture.drawRec(self.frame_rect, position, tint);
+    }
+};
+
 const app_name = "Desktop Pets";
 const window_icon_png = @embedFile("zig-favicon.png");
 
@@ -42,7 +73,7 @@ pub fn main(init: std.process.Init) !void {
     const arena: std.mem.Allocator = init.arena.allocator();
     const app_conf = try AppConfig.init(arena, init.io, init.environ_map, "Desktop Pets");
     defer app_conf.deinit();
-    errdefer std.debug.print("Data Directory: '{s}'\n", .{app_conf.data_dir});
+    std.debug.print("Data Directory: '{s}'\n", .{app_conf.data_dir});
 
     rl.setConfigFlags(.{
         .borderless_windowed_mode = true,
@@ -74,36 +105,32 @@ pub fn main(init: std.process.Init) !void {
     rl.minimizeWindow();
     rl.restoreWindow();
 
-    const texture_path = try std.fs.path.joinZ(arena, &.{ app_conf.data_dir, "scarfy.png" });
-    const scarfy = try rl.Texture.init(texture_path);
-    defer rl.unloadTexture(scarfy);
-    arena.free(texture_path);
+    const scarfy_path = try std.fs.path.joinZ(arena, &.{ app_conf.data_dir, "scarfy.png" });
+    var scarfy: SpriteAnim = try .init(scarfy_path, 6, 1, 6);
+    defer scarfy.deinit();
+    arena.free(scarfy_path);
 
-    const position = rl.Vector2.init(350.0, 280.0);
+    const slime_path = try std.fs.path.joinZ(arena, &.{ app_conf.data_dir, "slime_green.png" });
+    var slime: SpriteAnim = try .init(slime_path, 12, 3, 4);
+    defer slime.deinit();
+    arena.free(slime_path);
 
-    var frame_rect = rl.Rectangle{
-        .x = 0,
-        .y = 0,
-        .width = @as(f32, @floatFromInt(@divFloor(scarfy.width, 7))),
-        .height = @as(f32, @floatFromInt(scarfy.height)),
-    };
+    var scarfy_pos = rl.Vector2.init(350.0, 280.0);
+    const slime_pos = rl.Vector2.init(270.0, 420.0);
 
-    var current_frame: u32 = 0;
     var frames_counters: u32 = 0;
-    const frame_count = 5;
     const frames_speed = 6;
 
     rl.setTargetFPS(fps);
 
     while (!rl.windowShouldClose()) {
-
         // frame work
-
         frames_counters += 1;
         if (frames_counters >= (fps / frames_speed)) {
             frames_counters = 0;
-            current_frame = (current_frame + 1) % frame_count;
-            frame_rect.x = @as(f32, @floatFromInt(current_frame)) * @as(f32, @floatFromInt(@divFloor(scarfy.width, 6)));
+            scarfy.advance();
+            scarfy_pos.x = @rem(scarfy_pos.x + 10, @as(f32, @floatFromInt(rl.getRenderWidth())));
+            slime.advance();
         }
 
         rl.beginDrawing();
@@ -117,11 +144,12 @@ pub fn main(init: std.process.Init) !void {
 
                 dvui.label(@src(), "dvui works!", .{}, .{});
             }
-
-            rl.drawRectangleLines(border_margin, border_margin, rl.getRenderWidth() - border_margin, rl.getRenderHeight() - border_margin, .red);
             _ = try win.end(.{ .manage_backend = false });
 
-            scarfy.drawRec(frame_rect, position, .white); // Draw part of the texture
+            rl.drawRectangleLines(border_margin, border_margin, rl.getRenderWidth() - border_margin, rl.getRenderHeight() - border_margin, .red);
+
+            scarfy.draw(scarfy_pos, .white);
+            slime.draw(slime_pos, .white);
 
             rl.drawRectangle(100, 100, 100, 100, .sky_blue);
         }
